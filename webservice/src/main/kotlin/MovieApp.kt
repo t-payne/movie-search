64b5b1dee93921bc21e39e175614b3ecd767b3e7
Taylor Payne <tpayne.dev@gmail.com>
2019-01-10 13:52:24 -0700

    Set up basic movie app with Ktor framework
    Set up Gradle build to support Ktor.  Create basic Kotlin
    app that responds with "Movies App" when GET request is sent to
    localhost:8080.
    
    See https://ktor.io/quickstart/quickstart/gradle.html
    for setup details.

---
+++ webservice/src/main/kotlin/MovieApp.kt
@@ -0,0 +1,19 @@
+package movies
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main(args: Array<String>) {
+    embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("Movies App", ContentType.Text.Html)
+            }
+        }
+    }.start(wait = true)
+}
+
