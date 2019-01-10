f0b1b8756dc05e6711869658b1defee82541a7d2
Taylor Payne <tpayne.dev@gmail.com>
2019-01-10 15:26:51 -0700

    Add initial response to "movies" endpoint
    Handle routing in Application object. Respond to /movies endpoint with
    "Search for " and the value of the search parameter.

--- webservice/src/main/kotlin/MovieApp.kt
+++ webservice/src/main/kotlin/MovieApp.kt
@@ -1,19 +1,30 @@
 package movies
 
 import io.ktor.application.*
+import io.ktor.features.CallLogging
+import io.ktor.features.DefaultHeaders
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
-fun main(args: Array<String>) {
-    embeddedServer(Netty, 8080) {
-        routing {
-            get("/") {
-                call.respondText("Movies App", ContentType.Text.Html)
-            }
+fun Application.module() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    routing {
+        get("/") {
+            call.respondText("Movies App", ContentType.Text.Html)
         }
-    }.start(wait = true)
+        get("/movies") {
+            val title = call.parameters["search"]
+            call.respondText("Search for " + title, ContentType.Text.Html)
+        }
+    }
 }
+
+fun main(args: Array<String>) {
+    embeddedServer(Netty, 8080, watchPaths = listOf("MovieAppKt"), module = Application::module).start()
+}
+
 
