e7f237babf1d12781c89334602a6e12e393e0543
Taylor Payne <tpayne.dev@gmail.com>
2019-01-10 14:40:11 -0700

    Add first JUnit5 test
    Set up JUnit5 by adding dependencies in build.gradle. Add first
    JUnit5 Kotlin test file.
    
    Edit .gitignore to not ignore test files.

---
+++ webservice/src/test/kotlin/MovieAppTest.kt
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class MovieAppTest {
+
+    @DisplayName("TestMovieApp")
+    @Test
+    fun `First test`() {
+        println("Hello, JUnit5!")
+    }
+}
