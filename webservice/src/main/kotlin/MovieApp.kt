a1b0c7f729f47ceda2d6f57c68b69a2b65c090c2
Taylor Payne <tpayne.dev@gmail.com>
2019-01-16 14:13:57 -0700

    Add ability to search movies endpoint of TMDB API
    When a movie search GET request comes in, the app will
    use the search parameter passed in as the query parameter
    for an API call to the TMDB /search/movie endpoint.  When the
    result of the call comes back, the app returns the json result
    to the client.
    
    The TMDB API key is accessed through an environment variable.

--- webservice/src/main/kotlin/MovieApp.kt
+++ webservice/src/main/kotlin/MovieApp.kt
@@ -8,6 +8,30 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.MediaType
+
+
+fun fetchMovies(title: String?): String {
+    val apiKey: String = System.getenv("TMDB_API_KEY") ?: ""
+    val client = OkHttpClient()
+
+    val request = Request.Builder()
+        .url("https://api.themoviedb.org/3/search/movie?" +
+                "region=US&include_adult=false&page=1&query=" + title + "&language=en-US&api_key=" + apiKey)
+        .get()
+        .build()
+
+    val response = client.newCall(request).execute()
+    var responseBody = response?.body()?.string()
+    println(responseBody)
+
+    if (responseBody == null) responseBody = "Error retrieving movies"
+    return responseBody
+}
+
 
 fun Application.module() {
     install(DefaultHeaders)
@@ -18,7 +42,8 @@ fun Application.module() {
         }
         get("/movies") {
             val title = call.parameters["search"]
-            call.respondText("Search for " + title, ContentType.Text.Html)
+            val result = fetchMovies(title)
+            call.respondText(result, ContentType.Text.Html)
         }
     }
 }
